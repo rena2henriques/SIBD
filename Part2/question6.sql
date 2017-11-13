59a6c8909a6b4f1c5b3f5b85daa13db2a10194cc
mariana <mariana.galrinho@tecnico.ulisboa.pt>
2017-11-13 22:23:44 +0000

    Function region_overlaps_element working!

--- Part2/question6.sql
+++ Part2/question6.sql
@@ -5,14 +5,14 @@ of	the	element	A	overlaps	with	Region	B,	and	false	otherwise.	*/
 
 delimiter$$
 
-drop function if exists checkCoordinates();
+drop function if exists checkCoordinates;
 
 create function checkCoordinates(Ax1 FLOAT(4, 3), Ax2 FLOAT(4, 3), Ay1 FLOAT(4,3), Ay2 FLOAT(4,3), Bx1 FLOAT(4, 3), Bx2 FLOAT(4, 3), By1 FLOAT(4,3), By2 FLOAT(4,3))
-return BOOLEAN
+returns BOOLEAN
 begin
 	declare Aux FLOAT(4,3);
 
-	-- Conditions used to garantee if that x1 < x2 and y1 < y2
+	-- Conditions used to garantee that x1 < x2 and y1 < y2
 	if Ax1 > Ax2 then set Aux = Ax1; set Ax1 = Ax2; set Ax2 = Aux; end if;
 
 	if Bx1 > Bx2 then set Aux = Bx1; set Bx1 = Bx2; set Bx2 = Aux; end if;
@@ -23,26 +23,28 @@ begin
 
 	/* checking if the coordinates overlap */
 	/* we check the cases that they don't overlap instead of the ones they do, because there are less conditions*/
-	if Bx1 > Ax2 or Bx2 < Ax1 or By1 > Ay2 or By2 < By1 then
+	if Bx1 >= Ax2 or Bx2 <= Ax1 or By1 >= Ay2 or By2 <= Ay1 then
 		return FALSE; -- the regions don't overlap
 	else
 		return TRUE; -- the regions overlap
 	end if;
-end
+end $$
 
-drop function if exists region_overlaps_element();
 
-create function region_overlaps_element(series_id varchar(255), index int unsigned, x1 FLOAT(4, 3), x2 FLOAT(4, 3), y1 FLOAT(4, 3), y2 FLOAT(4, 3))
+drop function if exists region_overlaps_element;
+
+create function region_overlaps_element(idA int unsigned, indexA int unsigned, x1B FLOAT(4, 3), y1B FLOAT(4, 3), x2B FLOAT(4, 3), y2B FLOAT(4, 3))
 returns BOOLEAN
 begin
 	-- returns true if there is overlapping or false if there isn't
 	return exists (select r.series_id, r.elem_index 
 		from Region as r 
-		where r.series_id = series_id and r.elem_index = index and checkCoordinates(r.x1, r.x2, r.y1, r.y2, x1, x2, xy, x2));
+		where r.series_id = idA and r.elem_index = indexA and checkCoordinates(r.x1, r.x2, r.y1, r.y2, x1B, x2B, y1B, y2B));
 	-- checkCoordinates is a functions that tests if x1 < x2 and y1 < y2 and if the coordinates overlap
 end$$
 	
 delimiter;	
+
 
 
 /*Para construir a função de teste em relação à sobreposição de regiões considerou-se que as coordenadas do ponto 1 (x1, y1) não têm que 
