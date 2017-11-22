2dc5db058e4cc8d4c0564413493029f340981804
rena2henriques <renato.manao@gmail.com>
2017-11-22 00:40:51 +0000

    question 4, inserting regions works

--- Part3/renato/addregion.php
+++ Part3/renato/addregion.php
@@ -22,7 +22,26 @@
 				exit();
 			}
 
+			// get the region info
+			$series_id =$_REQUEST['seriesid'];
+			$elem_index =$_REQUEST['elem_index'];
+			$x1 =$_REQUEST['x1'];
+			$y1 =$_REQUEST['y1'];
+			$x2 =$_REQUEST['x2'];
+			$y2 =$_REQUEST['y2'];
+
 			// Inserir new region -> acho que é para inserir sempre
+
+			$stmt = $connection->prepare("INSERT INTO Region VALUES (:series_id,:elem_index,:x1,:y1,:x2,:y2)");
+
+			$stmt->bindParam(':series_id', $series_id);
+			$stmt->bindParam(':elem_index', $elem_index);
+			$stmt->bindParam(':x1', $x1);
+			$stmt->bindParam(':y1', $y1);
+			$stmt->bindParam(':x2', $x2);
+			$stmt->bindParam(':y2', $y2);
+
+			$stmt->execute();
 
 			// QUERY -> get all regions of an element from the last study of the patient (if there was one)
 
