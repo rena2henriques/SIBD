f04e1fc5b30079661cead776e71d3ce7b251e336
mariana <mariana.galrinho@tecnico.ulisboa.pt>
2017-11-27 12:02:52 +0000

    Now with drop down list for request nr

---
+++ Part3/formStudy.php
@@ -0,0 +1,71 @@
+
+<html>
+<head>
+	<title>Create a new study:</title>
+	<link href="https://fonts.googleapis.com/css?family=Indie+Flower" rel="stylesheet">
+</head>
+<body>
+	<h1 style="font-family: 'Indie Flower', cursive;">Clinic Database:</h1>
+
+	<form method="post" action="createStudy.php">
+		<fieldset style="width: 50%;">
+		<legend><strong>Register a new Study:</strong></legend>
+			<p><strong>Request Number:</strong>
+			<select name="requestnumber">
+<?php
+			$host = "db.ist.utl.pt";
+			$user = "ist181588";
+			$pass = "gjzf1955";
+			$dsn = "mysql:host=$host;dbname=$user";
+			try
+			{
+				$connection = new PDO($dsn, $user, $pass);
+			}
+			catch(PDOException $exception)
+			{
+				echo("<p>Error: ");
+				echo($exception->getMessage());
+				echo("</p>");
+				exit();
+			}
+			$sql = "SELECT number FROM Request ORDER BY number";
+			$result = $connection->query($sql);
+			if ($result == FALSE)
+			{
+				$info = $connection->errorInfo();
+				echo("<p>Error: {$info[2]}</p>");
+				exit();
+			}
+			foreach($result as $row)
+			{
+				$requestnumber = $row['requestnumber'];
+				echo("<option value=\"$requestnumber\">$requestnumber</option>");
+			}
+			$connection = null;
+?>
+			</select>
+			</p>
+			<input type="text" name="requestnumber" autofocus style="width: 50%;"  maxlength="30" placeholder="Request Number" required /><br></p>
+			<p><strong>Description:</strong>
+			<input type="text" name="description" autofocus style="width: 60%;" maxlength="255" placeholder="Description" required /><br></p>
+			<p><strong>Date:</strong>
+			<input type="text" name="date" autofocus style="width: 40%;" maxlength="20" placeholder="YEAR-MONTH-DAY HH:MM"/><br></p>
+			<p><strong>Doctor ID:</strong> 	
+			<input type="text" name="doctorid" autofocus style="width: 40%;" maxlength="30" placeholder="Doctor ID" /></p>
+			<p><strong>Manufacturer of Device:</strong> 	
+			<input type="text" name="manufacturer" autofocus style="width: 50%;" maxlength="255" placeholder="Manufacturer" /></p>
+			<p><strong>Serial Number of Device:</strong> 	
+			<input type="text" name="serialnumber" autofocus style="width: 40%;" maxlength="255" placeholder="Serial Number" /></p>
+			<p><strong>Series ID:</strong> 	
+			<input type="text" name="seriesid" autofocus style="width: 20%;" maxlength="30" placeholder="Series ID" required  /><p>
+			<p><strong>Series Name:</strong> 	
+			<input type="text" name="seriesname" autofocus style="width: 20%;" maxlength="30" placeholder="Series Name" /><p>
+			<input type="submit" value="Submit"/></p>
+		</fieldset>
+	</form>
+
+	<!-- Button to go to home page -->
+	<br><form action='checkPatient.html' method='post'>
+	<input type='submit' value='Home'/></form>
+</body>
+</html>
